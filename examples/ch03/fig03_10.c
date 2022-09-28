4bee2b0ded4449ef33779071fdfb96219a9379e9
bdamja <iceicebabyalter@gmail.com>
2022-09-28 15:00:59 -0600

    changes

--- examples/ch03/fig03_10.c
+++ examples/ch03/fig03_10.c
@@ -9,24 +9,27 @@ int main( void )
    unsigned int passes = 0; // number of passes   
    unsigned int failures = 0; // number of failures 
    unsigned int student = 1; // student counter    
-   int result; // one exam result 
+   int result = 0; // one exam result
 
    // process 10 students using counter-controlled loop 
    while ( student <= 10 ) {
-
+      int scannedValue = 0;
       // prompt user for input and obtain value from user 
       printf( "%s", "Enter result ( 1=pass,2=fail ): " );
-      scanf( "%d", &result );
+      scannedValue = scanf( "%d", &result );
+      while ((getchar() != '\n'));
 
-      // if result 1, increment passes 
-      if ( result == 1 ) {     
-         passes = passes + 1;
-      } // end if 
-      else { // otherwise, increment failures 
-         failures = failures + 1;
-      } // end else 
-
-      student = student + 1; // increment student counter  
+      if ( scannedValue == 1 ) { // if scanf() properly scanned an int
+         if ( ( result == 1 ) || ( result == 2 ) ) {
+            if ( result == 1 ) { // if result 1, increment passes
+               passes = passes + 1;
+            } // end if
+            else { // otherwise, increment failures
+               failures = failures + 1;
+            }
+            student = student + 1; // increment student counter, only if result is 1 or 2
+         }
+      }
    } // end while 
 
    // termination phase; display number of passes and failures 
